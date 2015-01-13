72db25da7322102aacbfe62dc091fec46a977487
LeonM234 <LeonM234@yahoo.com>
2015-01-13 12:18:44 -0800

    First Commit - needs refactoring

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello world!");
+
+            System.Console.WriteLine("What is your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("hey " + name);
+
+            //cheer the user on
+            //gimme a...
+            //BOB is GRAND!
+            for (int i = 0; i < name.Length; i++)
+            {
+                name = name.ToLower();
+                if (name[i].ToString() == "a" || name[i].ToString() == "e" || name[i].ToString() == "i" ||
+                    name[i].ToString() == "o" || name[i].ToString() == "u")
+                    System.Console.WriteLine("Give me a..." + naame[i]);
+            }
+            System.Console.WriteLine(name.ToUpper() + " is GRAND!");
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
